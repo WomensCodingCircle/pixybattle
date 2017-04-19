199c63a4218c0562748e709787cd4d7900d3adea
Konrad Rokicki <krokicki@gmail.com>
2017-04-18 21:47:30 -0400

    added pyserial library

--- install.sh
+++ install.sh
@@ -5,6 +5,9 @@ echo "Setting up Pixy environment..."
 DIR=$(cd "$(dirname "$0")"; pwd);
 echo "Base directory: $DIR"
 
+# Set keyboard to US layout
+L='us' && sudo sed -i 's/XKBLAYOUT=\"\w*"/XKBLAYOUT=\"'$L'\"/g' /etc/default/keyboard
+
 # Clone the pixy repo
 cd $DIR/lib
 git clone https://github.com/WomensCodingCircle/pixy.git
@@ -35,6 +38,12 @@ touch $TARGET_DIR/__init__.py
 cp $BUILD_DIR/pixy.py $TARGET_DIR
 cp $BUILD_DIR/_pixy.so $TARGET_DIR
 
-# Set keyboard to US layout
-L='us' && sudo sed -i 's/XKBLAYOUT=\"\w*"/XKBLAYOUT=\"'$L'\"/g' /etc/default/keyboard
+# Install PySerial for communication with Teensy
+cd $DIR/lib
+wget https://github.com/pyserial/pyserial/releases/download/v3.2.1/pyserial-3.2.1.tar.gz
+tar xvfz pyserial-3.2.1.tar.gz
+cd pyserial-3.2.1
+sudo python setup.py install
+
+echo "PixyBattle installation complete. Ready to fight!"
 
